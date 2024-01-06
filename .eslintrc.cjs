1721b302099b4fc08f2feaaa77fb77f7335d0349
Dhruva Agrawal <dhruva1998@gmail.com>
2024-01-06 22:03:40 +0530

    added shadcn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,47 +1,50 @@
 module.exports = {
-	$schema: "https://json.schemastore.org/eslintrc",
-	env: { browser: true, es2020: true },
-	root: true,
-	extends: [
-		"prettier",
-		"plugin:tailwindcss/recommended",
-		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:react-hooks/recommended",
-		"plugin:react/recommended",
-		"plugin:import/recommended",
-		"eslint-config-prettier",
-	],
-	parser: "@typescript-eslint/parser",
-	plugins: ["tailwindcss", "react-refresh"],
-	rules: {
-		"react/react-in-jsx-scope": "off",
-		"react/jsx-key": "off",
-		"tailwindcss/no-custom-classname": "off",
-		"tailwindcss/classnames-order": "off",
-		"react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-	},
-	settings: {
-		settings: {
-			react: {
-				version: "detect",
-			},
-			"import/resolver": {
-				node: {
-					paths: ["src"],
-					extensions: [".js", ".jsx", ".ts", ".tsx"],
-				},
-			},
-		},
-		tailwindcss: {
-			callees: ["cn"],
-			config: "tailwind.config.js",
-		},
-	},
-	overrides: [
-		{
-			files: ["*.ts", "*.tsx"],
-			parser: "@typescript-eslint/parser",
-		},
-	],
-};
+  $schema: "https://json.schemastore.org/eslintrc",
+  env: { browser: true, es2020: true },
+  root: true,
+  extends: [
+    "prettier",
+    "plugin:tailwindcss/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "eslint-config-prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["tailwindcss", "react-refresh"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-key": "off",
+    "tailwindcss/no-custom-classname": "off",
+    "tailwindcss/classnames-order": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+  settings: {
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: {
+          paths: ["src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      },
+    },
+    tailwindcss: {
+      callees: ["cn"],
+      config: "tailwind.config.js",
+    },
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
+}
