3d3c2745f92de1b2a33f3958468ddf202581aa5a
Dhruva Agrawal <dhruva1998@gmail.com>
2024-01-07 03:44:43 +0530

    made some more changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react"
+import App from "@/App.tsx"
 import { ClerkProvider } from "@clerk/clerk-react"
 import { neobrutalism } from "@clerk/themes"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import ReactDOM from "react-dom/client"
-
-import App from "./App.tsx"
 
 import "./index.css"
 
