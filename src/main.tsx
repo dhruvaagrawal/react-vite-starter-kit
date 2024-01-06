26119ecc7a210160c085d8290f3e63ebd41ed0c5
Dhruva Agrawal <dhruva1998@gmail.com>
2024-01-06 22:18:22 +0530

    added the env package and file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import { ClerkProvider } from "@clerk/clerk-react"
+import { neobrutalism } from "@clerk/themes"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import ReactDOM from "react-dom/client"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from "./App.tsx"
+
+import "./index.css"
+
+import { env } from "@/env"
+
+export const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ClerkProvider
+      publishableKey={env.VITE_CLERK_PUBLISHABLE_KEY}
+      appearance={{ baseTheme: neobrutalism }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ClerkProvider>
+  </React.StrictMode>
 )
